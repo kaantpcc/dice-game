d3ec0b29b928081d7b523fceed95dab0ffa7534c
kaantpcc <118486891+kaantpcc@users.noreply.github.com>
2023-08-14 17:32:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1=Math.floor((Math.random()*6) + 1);
+var randomImage1="./images/dice" + randomNumber1 + ".png";
+
+var randomNumber2=Math.floor((Math.random()*6) + 1);
+var randomImage2="./images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").setAttribute("src",randomImage1);
+document.querySelector(".img2").setAttribute("src",randomImage2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").textContent = " ! Player 1 WINS";
+}else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").textContent = "Player 2 WINS !";
+}else{
+    document.querySelector("h1").textContent = "DRAW";
+}
